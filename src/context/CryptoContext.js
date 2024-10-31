1ce9238cd1a0874649a2112866a9e3c471bf2a31
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-10-31 17:15:38 +0100

    context API

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useLayoutEffect, useState } from "react"
 
 
 
@@ -7,9 +7,26 @@ export const CryptoContext = createContext({})
 
 // Create the provider component
 export const CryptoProvider = ({children}) => {
-    const [test, setTest] = useState("This is test state");
+    const [cryptoData, setCryptoData] = useState();
+
+    const getCryptoData = async() => {
+
+        try {
+            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10&page=1&price_change_percentage=1h%2C24h%2C7d`)
+            .then(res => res.json())
+            .then(json => json)
+            console.log(data)
+           setCryptoData(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useLayoutEffect(() => {
+      getCryptoData();
+    }, [])
     return(
-        <CryptoContext.Provider value={{ test }}>
+        <CryptoContext.Provider value={{ cryptoData }}>
             {children}
         </CryptoContext.Provider>
     )
