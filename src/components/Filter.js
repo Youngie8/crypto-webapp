53e9dd47299e2a6d763abf5c4fb9b801d1a29acc
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-11-03 16:53:03 +0100

    created filter component

---
+++ src/components/Filter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div className='w-full h-12 border-2 border-gray-100 rounded-lg flex items-center justify-between relative'>
+        filter
+    </div>
+  )
+}
+
+export default Filter
--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import TableComponent from '../components/TableComponent'
+import Filter from '../components/Filter'
 
 const Crypto = () => {
   return (
     <section className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
+      <Filter />
         <TableComponent/>
     </section>
   )
