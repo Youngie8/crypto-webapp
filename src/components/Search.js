a4283fc803d43dd58ce3aefab81c6c25ba849335
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-11-12 22:46:06 +0100

    created search options

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchIcon from '../assets/search-icon.svg'
 const Search = () => {
+    const [searchText, setSearchText] = useState("");
+    let handleInput = (e) => {
+        e.preventDefault();
+        let query  =  e.target.value;
+        setSearchText(query)
+    }
   return (
-    <form className='border border-gray-100'>
-        <input type='text' name='search' 
-        className='bg-inherit'/>
-        <button type='submit'>
-            <img src={SearchIcon} alt='search'/>
-        </button>
-    </form>
+    <>
+        <form className='w-96 relative flex items-center ml-7 font-nunito'>
+            <input type='text' name='search' placeholder='search...' onChange={handleInput} value={searchText}
+            className='w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border-transparent focus:border-cyan'/>
+            <button type='submit' className='absolute right-1 cursor-pointer'>
+                <img src={SearchIcon} alt='search' className='w-full h-auto'/>
+            </button>
+        </form>
+
+        {
+            searchText.length > 0 ?
+
+            <ul className='absolute top-11 right-0 w-full h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 backdrop-blur-md'>
+                <li>Bitcoin</li>
+                <li>Bitcoin</li>
+            </ul>
+            : ""
+        }
+    </>
   )
 }
 
