7031be3645372c3b362ae894a04b72a8c8de9dab
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-11-14 15:13:24 +0100

    searching component updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import debounce from 'lodash.debounce'
 import SearchIcon from '../assets/search-icon.svg'
-const Search = () => {
+import { CryptoContext } from '../context/CryptoContext';
+
+const SearchInput = ({ handleSearch }) =>{
     const [searchText, setSearchText] = useState("");
+    let {searchData, setCoinSearch} = useContext(CryptoContext);
+
     let handleInput = (e) => {
         e.preventDefault();
         let query  =  e.target.value;
-        setSearchText(query)
+        setSearchText(query);
+        handleSearch(query)
     }
-  return (
-    <>
+
+    const selectCoin = (coin) => {
+        setCoinSearch(coin)
+    }
+    return(
+<>
         <form className='w-96 relative flex items-center ml-7 font-nunito'>
             <input type='text' name='search' placeholder='search...' onChange={handleInput} value={searchText}
             className='w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border-transparent focus:border-cyan'/>
@@ -20,13 +30,31 @@ const Search = () => {
         {
             searchText.length > 0 ?
 
-            <ul className='absolute top-11 right-0 w-full h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 backdrop-blur-md'>
-                <li>Bitcoin</li>
-                <li>Bitcoin</li>
+            <ul className='absolute top-11 right-0 w-96 h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 backdrop-blur-md'>
+                {
+                    searchData ? searchData.map(coin => {return <li className='flex items-center ml-4 my-2 cursor-pointer' key={coin.id} onClick={() => selectCoin(coin.id)}>
+                        <img className='w-[1rem] h-[1rem] mx-1.5' src={coin.thumb}  alt={coin.name}/>
+                        <span>{ coin.name }</span>
+                    </li>}) : <h2>Please Wait...</h2>
+                }
             </ul>
             : ""
         }
     </>
+    )
+}
+const Search = () => {
+    
+    let {getSearchResult} = useContext(CryptoContext)
+
+    const debounceFunc = debounce(function(val) {
+        getSearchResult(val)
+    }, 2000)
+    
+  return (
+    <div className='relative'>
+        <SearchInput handleSearch={debounceFunc}/>
+    </div>
   )
 }
 
--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -8,15 +8,33 @@ export const CryptoContext = createContext({})
 // Create the provider component
 export const CryptoProvider = ({children}) => {
     const [cryptoData, setCryptoData] = useState();
+    const [searchData, setSearchData] = useState();
+    const [coinSearch, setCoinSearch] = useState("");
+
+   
 
     const getCryptoData = async() => {
 
         try {
-            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10&page=1&price_change_percentage=1h%2C24h%2C7d`)
+            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSearch}&per_page=10&page=1&price_change_percentage=1h%2C24h%2C7d`)
+            .then(res => res.json())
+            .then(json => json)
+            // console.log(data)
+           setCryptoData(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const getSearchResult = async(query) => {
+
+        try {
+            const data = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`)
             .then(res => res.json())
             .then(json => json)
             console.log(data)
-           setCryptoData(data)
+           setSearchData(data.coins)
         } catch (error) {
             console.log(error)
         }
@@ -24,9 +42,9 @@ export const CryptoProvider = ({children}) => {
 
     useLayoutEffect(() => {
       getCryptoData();
-    }, [])
+    }, [coinSearch])
     return(
-        <CryptoContext.Provider value={{ cryptoData }}>
+        <CryptoContext.Provider value={{ cryptoData, searchData, getSearchResult, setCoinSearch }}>
             {children}
         </CryptoContext.Provider>
     )
